64cb62803b8f01b5a07a2c76e84b4b48db1bb8ca
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-01 15:06:20 +0300

    Proje dosyası ekle.

---
+++ Week4/Baby.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4
+{
+    public class Baby
+    {
+        string _name;
+        string _surName;
+        DateTime _birthDay;
+
+       public string name
+        {
+            get { return _name; }
+            set {
+                _name = value;
+                   }
+        }
+        public string surName
+        {
+            get { return _surName; }
+            set { _surName = value; }
+        }
+        public DateTime BirthDay
+        {
+        
+        
+            get { return _birthDay; }
+            set { _birthDay = value; }
+        }
+        public Baby() 
+        { 
+        }
+
+        public Baby(String name , String surName )
+        {
+            _name = name;
+            _surName = surName;
+            _birthDay = DateTime.Now;
+            Print();
+        }
+        public void Print()
+        {
+            Console.WriteLine("ıngaaa ");
+            Console.WriteLine($"Çocuğunuznun isimi {name} soyisimi {surName} ");
+            Console.WriteLine("Çocuğunun doğum tarihi "+ BirthDay);
+        }
+     }
+}
---
+++ Week4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+using Week4;
+
+Console.WriteLine("Hello, World!");
+
+Baby baby1 = new Baby("Sezen", "Aksu");
+
+Baby baby2 = new Baby();
+baby2.name = "Şebnem";
+baby2.surName = "Ferah";
+baby2.BirthDay=DateTime.Now;
+baby2.Print();
+
+
